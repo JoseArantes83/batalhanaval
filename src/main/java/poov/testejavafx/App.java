628338d37518c89c33c3b118b23245a29ba4cca7
José Ferreira Arantes Lopes <102867346+JoseArantes83@users.noreply.github.com>
2023-07-08 01:22:12 -0300

    k

--- src/main/java/poov/testejavafx/App.java
+++ src/main/java/poov/testejavafx/App.java
@@ -21,55 +21,19 @@ public class App extends Application {
     public static ArrayList<Navio> navios1;
     public static ArrayList<Navio> navios2;
     public static Status status;
-    public static int momento;
     public static int vezJogador;
-    public static boolean posicionando;
-    public static boolean posicionamento1;
-    public static boolean posicionamento2;
     public static int cont = 0;
 
     @Override
     public void start(Stage stage) throws IOException {
-        // status = Status.INICIALIZACAO;
+        status = Status.INICIALIZACAO;
         inicializacao();
 
-        // posicionando = false;
         status = Status.POSICIONAMENTO;
         vezJogador = 1;
         scene = new Scene(loadFXML("tabuleiro1"));
         stage.setScene(scene);
         stage.show();
-        /*
-         * 
-         * scene2 = new Scene(loadFXML("tabuleiro2"));
-         * 
-         * //setar botões do tabuleiro 1
-         * /*
-         * while (posicionando == true) {
-         * 
-         * while (vezJogador == 1) {
-         * // posicionamento jogador 1
-         * // Mostrando o tabuleiro 1
-         * 
-         * // verificação se a vez do 2 já acabou é feita no handle
-         * if (vezJogador == 2) {
-         * scene2 = new Scene(loadFXML("tabuleiro2"));
-         * stage.setScene(scene2);
-         * stage.show();
-         * 
-         * //setar botões do tabuleiro 2
-         * }
-         * }
-         * 
-         * while (vezJogador == 2) {
-         * 
-         * }
-         * }
-         */
-
-        // Aqui é um sequencial que posso usar como orientação do jogo
-        // ou tenho que fazer tudo orientado a eventos no handle?
-
     }
 
     static void setRoot(String fxml) throws IOException {
